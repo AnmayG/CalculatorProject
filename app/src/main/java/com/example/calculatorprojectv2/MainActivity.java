8c84981120a550a9349369bf3e73cd3f62ab6600
Anmay Gupta <anmaygupta1@gmail.com>
2021-09-08 11:46:45 -0500

    Clean up animation and files from version increase

--- app/src/main/java/com/example/calculatorprojectv2/MainActivity.java
+++ app/src/main/java/com/example/calculatorprojectv2/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.calculatorprojectv2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,10 +18,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Animation animationIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
-//        Animation animationOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
-        //^^Fade in and Fade out animation through XML files
-
         mainButton = (Button) findViewById(R.id.mainButton);
         nextButton = (Button) findViewById(R.id.nextButton);
 
@@ -30,52 +25,30 @@ public class MainActivity extends AppCompatActivity {
 
         final int[] stage = {0};
 
-        mainButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openLevelOne();
-            }
-        });
+        mainButton.setOnClickListener(view -> openLevelOne());
 
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (stage[0] == 0){
-                    instructionsLabel.setText("You're bored in class one day, so you take out your calculator");
-                } else if (stage[0] == 1){
-                    instructionsLabel.setText("But you notice something's a little off about it...");
-                } else if (stage[0] == 2){
-                    instructionsLabel.setText("YOUR CALCULATOR IS BROKEN!");
-                } else if (stage[0] == 3){
-                    instructionsLabel.setText("But since you are the civilized person that you are");
-                } else if (stage[0] == 4){
-                    instructionsLabel.setText("Instead of panicking, you decide to take this opportunity to cure your boredom!");
-                } else if (stage[0] == 5){
-                    instructionsLabel.setText("Your goal, is to get to the goal number, while staying under the button click limit");
-                } else if (stage[0] == 6){
-                    nextButton.setVisibility(View.GONE);
-                    instructionsLabel.setText("Press the Play Button to Start!");
-                    mainButton.setVisibility(View.VISIBLE);
-                }
-
-                stage[0]++;
+        nextButton.setOnClickListener(view -> {
+            if (stage[0] == 0){
+                instructionsLabel.setText("You're bored in class one day, so you take out your calculator");
+            } else if (stage[0] == 1){
+                instructionsLabel.setText("But you notice something's a little off about it...");
+            } else if (stage[0] == 2){
+                instructionsLabel.setText("YOUR CALCULATOR IS BROKEN!");
+            } else if (stage[0] == 3){
+                instructionsLabel.setText("But since you are the civilized person that you are");
+            } else if (stage[0] == 4){
+                instructionsLabel.setText("Instead of panicking, you decide to take this opportunity to cure your boredom!");
+            } else if (stage[0] == 5){
+                instructionsLabel.setText("Your goal, is to get to the goal number, while staying under the button click limit");
+            } else if (stage[0] == 6){
+                nextButton.setVisibility(View.GONE);
+                instructionsLabel.setText("Press the Play Button to Start!");
+                mainButton.setVisibility(View.VISIBLE);
             }
+
+            stage[0]++;
         });
     }
-
-    protected AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
-    protected AlphaAnimation fadeOut = new AlphaAnimation(1.0f, 0.0f);
-
-    private void fadeAnimation(TextView input){
-        input.startAnimation(fadeIn);
-        input.startAnimation(fadeOut);
-        fadeIn.setDuration(1000);
-        fadeOut.setDuration(1000);
-        fadeIn.setFillAfter(true);
-        fadeOut.setFillAfter(true);
-        fadeOut.setStartOffset(4200 + fadeIn.getStartOffset());
-    }
-    //^^ Fade animation (play around with later)
 
     public void openLevelOne(){
         Intent intent = new Intent(this, LevelOneActivity.class);
