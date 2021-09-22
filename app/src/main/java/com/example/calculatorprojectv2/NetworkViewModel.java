159ec059ea0b7d6e80b90cd12e342026e46e38bc
Anmay Gupta <anmaygupta1@gmail.com>
2021-09-22 11:40:39 -0500

    start work on multiple devices

--- app/src/main/java/com/example/calculatorprojectv2/NetworkViewModel.java
+++ app/src/main/java/com/example/calculatorprojectv2/NetworkViewModel.java
@@ -3,8 +3,13 @@ package com.example.calculatorprojectv2;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class NetworkViewModel extends ViewModel {
     private final MutableLiveData<Integer> currentLevel = new MutableLiveData<>(0);
+    private final MutableLiveData<ArrayList<Integer>> companionLevels = new MutableLiveData<>(new ArrayList<>());
+    private final MutableLiveData<ArrayList<String>> companionNames = new MutableLiveData<>(new ArrayList<>());
     private final MutableLiveData<Integer> points = new MutableLiveData<>(0);
     private final MutableLiveData<String> leader = new MutableLiveData<>("");
 
@@ -20,6 +25,14 @@ public class NetworkViewModel extends ViewModel {
         return leader;
     }
 
+    public MutableLiveData<ArrayList<Integer>> getCompanionLevels() {
+        return companionLevels;
+    }
+
+    public MutableLiveData<ArrayList<String>> getCompanionNames() {
+        return companionNames;
+    }
+
     public void setCurrentLevel(int level) {
         currentLevel.setValue(level);
     }
@@ -32,9 +45,32 @@ public class NetworkViewModel extends ViewModel {
         leader.setValue(lead);
     }
 
+    public void setCompanionLevels(ArrayList<Integer> levels) {
+        companionLevels.setValue(levels);
+    }
+
+    public void setCompanionLevels(int level, int index) {
+        Objects.requireNonNull(companionLevels.getValue()).set(index, level);
+    }
+
+    public void setCompanionLevels(int level, String name) {
+        int index = Objects.requireNonNull(companionNames.getValue()).indexOf(name);
+        setCompanionLevels(level, index);
+    }
+
+    public void setCompanionNames(ArrayList<String> names) {
+        companionNames.setValue(names);
+    }
+
+    public void setCompanionNames(String name, int index) {
+        Objects.requireNonNull(companionNames.getValue()).set(index, name);
+    }
+
     public void reset() {
         this.setCurrentLevel(0);
         this.setPoints(0);
         this.setLeader("");
+        this.setCompanionLevels(new ArrayList<>());
+        this.setCompanionNames(new ArrayList<>());
     }
 }
